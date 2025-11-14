2cb1c814c57b6e5925b2e6b2a1ca465532012f7b
PURPLE-ORCA <medms2005@gmail.com>
2025-11-14 15:45:02 +0100

    Fixed failing import

--- src/lib/hooks/useStoreUser.ts
+++ src/lib/hooks/useStoreUser.ts
@@ -2,7 +2,7 @@ import { useConvexAuth } from "convex/react";
 import { useMutation } from "convex/react";
 import { useEffect, useState } from "react";
 import { api } from "../../../convex/_generated/api";
-import { Id } from "../../convex/_generated/dataModel";
+import { Id } from "../../../convex/_generated/dataModel";
 
 export const useStoreUser = () => {
   const { isAuthenticated } = useConvexAuth();
